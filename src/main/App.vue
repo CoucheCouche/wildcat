14709feb80ab839810a9d71c1115e792404317f6
KimTaeHoon <wwwholakr@gmail.com>
2018-04-13 00:47:18 +0900

    init Setting, middleware 추가 필요

--- src/main/App.vue
+++ src/main/App.vue
@@ -1,12 +1,13 @@
 <template>
-    <div id="app">abc</div>
+    <div id="app">
+        <h1>Project Couche Couche</h1>
+    </div>
 </template>
-
 <script>
 export default {
     name: 'App',
     data() {
         return {};
-    },
+    }
 };
 </script>
